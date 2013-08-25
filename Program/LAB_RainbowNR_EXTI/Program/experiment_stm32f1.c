343f61737f92d670dd00c7236c7c9755f3487bfd
Hom <Hom19910422@gmail.com>
2013-08-26 01:21:04 +0800

    Update EXTI

---
+++ Program/LAB_RainbowNR_EXTI/Program/experiment_stm32f1.c
@@ -0,0 +1,89 @@
+/*=====================================================================================================*/
+/*=====================================================================================================*/
+#include "stm32f1_system.h"
+/*=====================================================================================================*/
+/*=====================================================================================================*/
+#define LED_B   PCO(13)
+#define LED_G   PCO(14)
+#define LED_R   PCO(15)
+
+#define KEY_WU  PAI(0)
+#define KEY_BO  PBI(2)
+/*=====================================================================================================*/
+/*=====================================================================================================*/
+void GPIO_Config( void );
+void EXTI_Config( void );
+/*=====================================================================================================*/
+/*=====================================================================================================*/
+int main( void )
+{
+ 	SystemInit();
+	GPIO_Config();
+  EXTI_Config();
+
+	while(1) {
+    LED_R = ~LED_R;
+    Delay_10ms(10);
+    if(KEY_BO == 1) {
+      LED_G = ~LED_G;
+      Delay_10ms(10);
+    }
+	}
+}
+/*=====================================================================================================*/
+/*=====================================================================================================*/
+void GPIO_Config( void )
+{
+  GPIO_InitTypeDef GPIO_InitStruct;
+
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA | RCC_APB2Periph_GPIOB | RCC_APB2Periph_GPIOC, ENABLE);
+
+  /* PC13 LED_B */	/* PC14 LED_G */	/* PC15 LED_R */
+  GPIO_InitStruct.GPIO_Pin = GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15;
+  GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
+  GPIO_InitStruct.GPIO_Mode = GPIO_Mode_Out_PP;
+  GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+  /* KEY_WU PA0 */	/* KEY_BO PB2 */
+  GPIO_InitStruct.GPIO_Pin = GPIO_Pin_0;
+  GPIO_InitStruct.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+  GPIO_Init(GPIOA, &GPIO_InitStruct);
+  GPIO_InitStruct.GPIO_Pin = GPIO_Pin_2;
+  GPIO_InitStruct.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+  GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  LED_R = 0;
+  LED_G = 0;
+  LED_B = 0;
+}
+/*=====================================================================================================*/
+/*=====================================================================================================*/
+void EXTI_Config( void )
+{
+  GPIO_InitTypeDef GPIO_InitStruct;
+  EXTI_InitTypeDef EXTI_InitStruct;
+  NVIC_InitTypeDef NVIC_InitStruct;
+
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO, ENABLE);
+
+  GPIO_InitStruct.GPIO_Pin = GPIO_Pin_0;
+  GPIO_InitStruct.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+  GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  GPIO_EXTILineConfig(GPIO_PortSourceGPIOA, GPIO_PinSource0);
+
+  EXTI_InitStruct.EXTI_Line = EXTI_Line0;
+  EXTI_InitStruct.EXTI_Mode = EXTI_Mode_Interrupt;
+  EXTI_InitStruct.EXTI_Trigger = EXTI_Trigger_Rising;
+  EXTI_InitStruct.EXTI_LineCmd = ENABLE;
+  EXTI_Init(&EXTI_InitStruct);
+
+  NVIC_InitStruct.NVIC_IRQChannel = EXTI0_IRQn;
+  NVIC_InitStruct.NVIC_IRQChannelPreemptionPriority = 0x0F;
+  NVIC_InitStruct.NVIC_IRQChannelSubPriority = 0x0F;
+  NVIC_InitStruct.NVIC_IRQChannelCmd = ENABLE;
+  NVIC_Init(&NVIC_InitStruct);
+}
+/*=====================================================================================================*/
+/*=====================================================================================================*/
