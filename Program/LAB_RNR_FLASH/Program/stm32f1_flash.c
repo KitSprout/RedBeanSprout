9b07eb3e23aedfaaf4ff1971e2222d6014a13b6a
Hom <Hom19910422@gmail.com>
2013-08-31 20:30:25 +0800

    Update FLASH

--- Program/LAB_RNR_FLASH/Program/stm32f1_flash.c
+++ Program/LAB_RNR_FLASH/Program/stm32f1_flash.c
@@ -4,14 +4,38 @@
 #include "stm32f1_flash.h"
 /*=====================================================================================================*/
 /*=====================================================================================================*
-**函數 : Flash_WritePage
-**功能 : 
-**輸入 : 
+**函數 : Flash_WritePageU8
+**功能 : U8 寫入 FLASH
+**輸入 : WritePage, *WriteData, DataLen
 **輸出 : None
-**使用 : 
+**使用 : Flash_WritePageU8(FLASH_PAGE_ADDR(12), WriteData, 1024);
 **=====================================================================================================*/
 /*=====================================================================================================*/
-void Flash_WritePage( u32 WritePage, uc16 *WriteData, u16 DataLen )
+void Flash_WritePageU8( u32 WritePage, uc8 *WriteData, u16 DataLen )
+{
+  u16 Count = 0;
+  FLASH_Status FLASHStatus;
+
+  FLASH_UnlockBank1();
+
+  FLASH_ClearFlag(FLASH_FLAG_EOP | FLASH_FLAG_PGERR | FLASH_FLAG_WRPRTERR);
+  FLASHStatus = FLASH_ErasePage(WritePage);
+  while((Count < DataLen) && (FLASHStatus == FLASH_COMPLETE)) {
+    FLASHStatus = FLASH_ProgramHalfWord(WritePage+Count, Byte16(WriteData[Count], WriteData[Count+1]));
+    Count += 2;
+  }
+  FLASH_LockBank1();
+}
+/*=====================================================================================================*/
+/*=====================================================================================================*
+**函數 : Flash_WritePageU16
+**功能 : U16 寫入 FLASH
+**輸入 : WritePage, *WriteData, DataLen
+**輸出 : None
+**使用 : Flash_WritePageU16(FLASH_PAGE_ADDR(12), WriteData, 512);
+**=====================================================================================================*/
+/*=====================================================================================================*/
+void Flash_WritePageU16( u32 WritePage, uc16 *WriteData, u16 DataLen )
 {
   u16 Count = 0;
   FLASH_Status FLASHStatus;
@@ -28,14 +52,34 @@ void Flash_WritePage( u32 WritePage, uc16 *WriteData, u16 DataLen )
 }
 /*=====================================================================================================*/
 /*=====================================================================================================*
-**函數 : Flash_ReadPage
-**功能 : 
-**輸入 : 
+**函數 : Flash_ReadPageU8
+**功能 : U8 讀取 FLASH
+**輸入 : ReadPage, *ReadData, DataLen
 **輸出 : None
-**使用 : 
+**使用 : Flash_ReadPageU8(FLASH_PAGE_ADDR(12), ReadData, 1024);
 **=====================================================================================================*/
 /*=====================================================================================================*/
-void Flash_ReadPage( u32 ReadPage, u16 *ReadData, u16 DataLen )
+void Flash_ReadPageU8( u32 ReadPage, u8 *ReadData, u16 DataLen )
+{
+  u16 Count = 0;
+  u16 ReadBuf = 0;
+
+  while(Count<DataLen) {
+    ReadBuf = (u16)(*(vu32*)(ReadPage+Count));
+    ReadData[Count++] = Byte8H(ReadBuf);
+    ReadData[Count++] = Byte8L(ReadBuf);
+  }
+}
+/*=====================================================================================================*/
+/*=====================================================================================================*
+**函數 : Flash_ReadPageU16
+**功能 : U16 讀取 FLASH
+**輸入 : ReadPage, *ReadData, DataLen
+**輸出 : None
+**使用 : Flash_ReadPageU16(FLASH_PAGE_ADDR(12), ReadData, 512);
+**=====================================================================================================*/
+/*=====================================================================================================*/
+void Flash_ReadPageU16( u32 ReadPage, u16 *ReadData, u16 DataLen )
 {
   u16 Count = 0;
 
@@ -47,10 +91,10 @@ void Flash_ReadPage( u32 ReadPage, u16 *ReadData, u16 DataLen )
 /*=====================================================================================================*/
 /*=====================================================================================================*
 **函數 : Flash_ErasePage
-**功能 : 
-**輸入 : 
+**功能 : 擦除 FLASH
+**輸入 : ErasePage
 **輸出 : None
-**使用 : 
+**使用 : Flash_ErasePage(FLASH_PAGE_ADDR(12));
 **=====================================================================================================*/
 /*=====================================================================================================*/
 void Flash_ErasePage( u32 ErasePage )
--- Program/LAB_RNR_FLASH/Program/stm32f1_flash.h
+++ Program/LAB_RNR_FLASH/Program/stm32f1_flash.h
@@ -7,13 +7,13 @@
 // Medium Density ... STM32F103x8(64KB), STM32F103xB(128KB)
 #define FLASH_START_ADDR        ((u32)0x08000000)
 #define FLASH_PAGE_SIZE         ((u16)0x0400)   // 1KBytes
-#define FLASH_PAGE_ADDR(PAGE)   ((u32)(FLASH_START_ADDR + FLASH_PAGE_SIZE*PAGE))
-
-#define FLASH_BOOTLOAD_ADDR     ((u32)0x1FFFF000)   // 2KB, 0x1FFF_F000 - 0x1FFF_F7FF
+#define FLASH_PAGE_ADDR(PAGE)   ((u32)(FLASH_START_ADDR + FLASH_PAGE_SIZE*(PAGE)))
 /*=====================================================================================================*/
 /*=====================================================================================================*/
-void Flash_WritePage( u32 WritePage, uc16 *WriteData, u16 DataLen );
-void Flash_ReadPage( u32 ReadPage, u16 *ReadData, u16 DataLen );
+void Flash_WritePageU8( u32 WritePage, uc8 *WriteData, u16 DataLen );
+void Flash_WritePageU16( u32 WritePage, uc16 *WriteData, u16 DataLen );
+void Flash_ReadPageU8( u32 ReadPage, u8 *ReadData, u16 DataLen );
+void Flash_ReadPageU16( u32 ReadPage, u16 *ReadData, u16 DataLen );
 void Flash_ErasePage( u32 ErasePage );
 /*=====================================================================================================*/
 /*=====================================================================================================*/
