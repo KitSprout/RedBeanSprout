58314ee1b799921be89c8b99fd74309e28393e64
Hom <Hom19910422@gmail.com>
2015-02-19 18:53:49 +0800

    Update TestFLASH 02/19

---
+++ Software/BSR_TestFLASH/ProjectBSR/RTE/RTE_Components.h
@@ -0,0 +1,18 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'ProjectSTM32' 
+ * Target:  'STM32F103C' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+#define RTE_DEVICE_STDPERIPH_FLASH
+#define RTE_DEVICE_STDPERIPH_FRAMEWORK
+#define RTE_DEVICE_STDPERIPH_GPIO
+#define RTE_DEVICE_STDPERIPH_RCC
+
+#endif /* RTE_COMPONENTS_H */
