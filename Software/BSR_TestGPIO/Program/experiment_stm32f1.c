75d08051a8ddd220975aaf5b61277a1c306c4225
Hom <Hom19910422@gmail.com>
2015-02-19 16:26:13 +0800

    Update TestGPIO 02/19

--- Software/BSR_TestGPIO/Program/experiment_stm32f1.c
+++ Software/BSR_TestGPIO/Program/experiment_stm32f1.c
@@ -53,16 +53,17 @@ void GPIO_Config( void )
 {
   GPIO_InitTypeDef GPIO_InitStruct;
 
+  /* Clk Init *************************************************************/
   RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA | RCC_APB2Periph_GPIOB | RCC_APB2Periph_GPIOC | RCC_APB2Periph_AFIO, ENABLE);
   GPIO_PinRemapConfig(GPIO_Remap_SWJ_JTAGDisable, ENABLE);
 
-  /* LED_B PC13 */	/* LED_G PC14 */	/* LED_R PC15 */
+  /* LED_B PC13 */  /* LED_G PC14 */  /* LED_R PC15 */
   GPIO_InitStruct.GPIO_Pin = GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15;
   GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
   GPIO_InitStruct.GPIO_Mode = GPIO_Mode_Out_PP;
   GPIO_Init(GPIOC, &GPIO_InitStruct);
 
-  /* KEY_WU PA0 */	/* KEY_BO PB2 */
+  /* KEY_WU PA0 */  /* KEY_BO PB2 */
   GPIO_InitStruct.GPIO_Pin = GPIO_Pin_0;
   GPIO_InitStruct.GPIO_Mode = GPIO_Mode_IN_FLOATING;
   GPIO_Init(GPIOA, &GPIO_InitStruct);
