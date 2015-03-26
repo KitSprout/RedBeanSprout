33dd6cdd127c1a2c3cfeb3a09ca5f30322fdd3d3
Hom <Hom19910422@gmail.com>
2015-03-27 00:33:24 +0800

    Add BSR_M4_Peripheral_UART_IntRecv 03/27

---
+++ SoftwareM4/BSR_M4_Peripheral_UART_IntRecv/Program/experiment_stm32f3.c
@@ -0,0 +1,55 @@
+/*====================================================================================================*/
+/*====================================================================================================*/
+#include <stdio.h>
+
+#include "stm32f3_system.h"
+#include "experiment_stm32f3.h"
+#include "module_rs232.h"
+/*====================================================================================================*/
+/*====================================================================================================*/
+int main( void )
+{
+  GPIO_Config();
+  RS232_Config();
+
+  LED_R_Reset;
+  printf("\r\nHello World!\r\n\r\n");
+
+  while(1) {
+    LED_G_Toggle;
+    Delay_100ms(1);
+  }
+}
+/*====================================================================================================*/
+/*====================================================================================================*/
+void GPIO_Config( void )
+{
+  GPIO_InitTypeDef GPIO_InitStruct;
+
+  /* GPIO Clk Init *************************************************************/
+  RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOA | RCC_AHBPeriph_GPIOB | RCC_AHBPeriph_GPIOC, ENABLE);
+
+  /* LED_B PC13 */  /* LED_G PC14 */  /* LED_R PC15 */
+  GPIO_InitStruct.GPIO_Pin = GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15;
+  GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
+  GPIO_InitStruct.GPIO_Mode = GPIO_Mode_OUT;
+  GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
+  GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_UP;
+  GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+  /* KEY_WU PA0 */  /* KEY_BO PB2 */
+  GPIO_InitStruct.GPIO_Pin = GPIO_Pin_0;
+  GPIO_InitStruct.GPIO_Mode = GPIO_Mode_IN;
+  GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_DOWN;
+  GPIO_Init(GPIOA, &GPIO_InitStruct);
+  GPIO_InitStruct.GPIO_Pin = GPIO_Pin_2;
+  GPIO_InitStruct.GPIO_Mode = GPIO_Mode_IN;
+  GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_DOWN;
+  GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  LED_R_Set;
+  LED_G_Set;
+  LED_B_Set;
+}
+/*====================================================================================================*/
+/*====================================================================================================*/
